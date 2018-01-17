901fa8823897f57ae2516311e6c3a19bdb2c2981
anan44 <t95371@gmail.com>
2018-01-17 20:54:30 +0100

    initial commit

---
+++ survey/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class SurveyPoint(models.Model):
+    """Individual survey point for data collection
+    """
+    name = models.CharField(max_length=255,
+                            unique=True)
+    x_Coordinate = models.FloatField()
+    y_Coordinate = models.FloatField()
+
+
+class Observation(models.Model):
+    """Individual weather Observation on a single SurveyPoint
+    """
+    temperature = models.IntegerField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    survey_point = models.ForeignKey(SurveyPoint,
+                                     on_delte=models.CASCADE)
