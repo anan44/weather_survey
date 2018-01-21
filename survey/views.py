7360437375cb5201b15a825ff0a82446008763bc
anan44 <t95371@gmail.com>
2018-01-21 14:27:31 +0100

    cleaned up the code a bit

--- survey/views.py
+++ survey/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from braces.views import SelectRelatedMixin
 
-from . import models
+from .models import Observation
 # Create your views here.
 
 
@@ -10,12 +10,12 @@ class CreateObservation(SelectRelatedMixin, generic.CreateView):
     """View to create the actual observations in the available cities
     """
     fields = ("temperature", "survey_point")
-    model = models.Observation
+    model = Observation
     success_url = reverse_lazy("thanks")
 
 
 class ListObservatsions(generic.ListView):
     """View for presenting all the data gathered ever
     """
-    model = models.Observation
+    model = Observation
     ordering = ["-time_stamp"]
