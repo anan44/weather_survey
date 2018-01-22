c5519ca0232a8156205363f7990842b46612341e
anan44 <t95371@gmail.com>
2018-01-22 21:35:29 +0100

    added staticdir to settings

--- weathersurvey/settings.py
+++ weathersurvey/settings.py
@@ -16,6 +16,7 @@ from .secret_settings import REALLY_SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
