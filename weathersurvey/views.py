60cf96b89b169c494be9ecf42e278e5e0268aba1
anan44 <t95371@gmail.com>
2018-01-23 22:58:03 +0100

    slight layout fix on homepage

--- weathersurvey/views.py
+++ weathersurvey/views.py
@@ -1,10 +1,36 @@
+from survey.models import Observation, SurveyPoint
 from django.views.generic import TemplateView
 # Create your views here.
 
 
 class HomePage(TemplateView):
+    """view presentting the home page and latest observations for each city
+    on it
+    """
     template_name = "index.html"
+    model = Observation
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomePage, self).get_context_data(**kwargs)
+        cities = [city.name for city in SurveyPoint.objects.all()]
+
+        def last_obs(city):
+            obs = Observation.objects.filter(survey_point__name=city)
+            if obs:
+                return obs.last()
+            else:
+                return {"survey_point": city,
+                        "temperature": "No data. Please help us by \
+                        contributing"}
+
+        data = [last_obs(city) for city in cities]
+        context["data"] = data
+
+        print(data)
+        return context
 
 
 class ThanksPage(TemplateView):
+    """Very basic Thanks view
+    """
     template_name = "thanks.html"
